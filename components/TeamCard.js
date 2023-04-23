637f6a2dfae9c159564e7bfc9f4b64ed9155d434
Demansell <demansell2016@gmail.com>
2023-04-22 23:03:03 -0500

    updated navbar got profile tab working and added teams card and indexing

---
+++ components/TeamCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSingleTeam } from '../api/teams';
+
+function TeamCard({ teamObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE BOOK AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE BOOKS
+  const deleteThisTeam = () => {
+    if (window.confirm(`Delete ${teamObj.team_name}?`)) {
+      deleteSingleTeam(teamObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px', display: 'flex' }}>
+      {/* <Card.Img variant="top" src={authorObj.image} alt={authorObj.title} style={{ height: '400px' }} /> */}
+      <Card.Body>
+        <Card.Title>{teamObj.team_name}</Card.Title>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS */}
+        <Link href={`/teams/${teamObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/author/edit/${teamObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisTeam} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+TeamCard.propTypes = {
+  teamObj: PropTypes.shape({
+    team_name: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default TeamCard;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import { getTeams } from '../api/teams';
+import TeamCard from '../components/TeamCard';
 
 function Home() {
+  // TODO: Set a state for books
+  const [teams, setTeam] = useState([]);
+
+  // TODO: Get user ID using useAuth Hook
   const { user } = useAuth();
 
+  // TODO: create a function that makes the API call to get all the books
+  const getAllTheTeams = () => {
+    getTeams(user.uid).then(setTeam);
+  };
+
+  // TODO: make the call to the API to get all the books on component render
+  useEffect(() => {
+    getAllTheTeams();
+  }, []);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+    <div className="text-center my-4">
+      <Link href="/teams/new" passHref>
+        <Button>Add A Team</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over books here using BookCard component */}
+        {teams.map((team) => (
+          <TeamCard key={team.firebaseKey} teamObj={team} onUpdate={getAllTheTeams} />
+        ))}
+      </div>
+
     </div>
   );
 }
