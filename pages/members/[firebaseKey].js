1924c0bef0dbe0c8541155891ad273e0ab5d6e36
Demansell <demansell2016@gmail.com>
2023-04-23 02:14:47 -0500

    got views page working but still broke, also got signout button working on nav

--- pages/members/[firebaseKey].js
+++ pages/members/[firebaseKey].js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { viewMemberDetails } from '../../../api/mergedData';
+import { viewMemberDetails } from '../../api/mergedData';
 
 export default function ViewMember() {
   const [memberDetails, setMemberDetails] = useState({});
@@ -18,16 +18,19 @@ export default function ViewMember() {
   return (
     <div className="mt-5 d-flex flex-wrap">
       <div className="d-flex flex-column">
-        <img src={memberDetails.image} alt={memberDetails.title} style={{ width: '300px' }} />
+        <img src={memberDetails.image} alt="" style={{ width: '300px' }} />
       </div>
       <div className="text-white ms-5 details">
         <h5>
-          {memberDetails.title} by {memberDetails.teamObject?.team_name}
+          Name: {memberDetails.name}
         </h5>
         <h5>
-          {memberDetails.role}
+          Role: {memberDetails.role}
         </h5>
-        Teams <a href={`mailto:${memberDetails.teamObject?.role}`}>{memberDetails.teamObject?.role}</a>
+        <h5>
+          Team Name: {memberDetails.teamObject?.team_name}
+        </h5>
+        <a href={`mailto:${memberDetails.teamObject?.role}`}>{memberDetails.teamObject?.role}</a>
       </div>
     </div>
   );
